43904116a1a7b8c15d321e2a1809c0d429b35e5a
Link1515 <h26281660@gmail.com>
2025-02-21 21:12:20 +0800

    implement circular queue

--- Queue/ArrayQueue.java
+++ Queue/ArrayQueue.java
@@ -1,20 +1,23 @@
 package Queue;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+// 環形陣列
+// 此實現會空出一個空間無法使用，例如傳入 maxSize = 4，但實際可使用的空間為 3
 public class ArrayQueue {
     private int maxSize;
-    private int front;
-    private int rear;
+    private int front; // 對列中的第一個元素位置，預設為 0
+    private int rear; // 對列中的最後一個元素後一個位置(空出一個空間)，預設為 0
     private int[] arr;
 
     public ArrayQueue(int maxSize) {
         this.maxSize = maxSize;
         arr = new int[this.maxSize];
-        front = -1;
-        rear = -1;
     }
 
     public boolean isFull() {
-        return rear == maxSize - 1;
+        return (rear + 1) % maxSize == front % maxSize;
     }
 
     public boolean isEmpty() {
@@ -26,16 +29,17 @@ public class ArrayQueue {
             System.out.println("對列已滿，不能加入");
             return;
         }
-        rear++;
         arr[rear] = n;
+        rear = (rear + 1) % maxSize;
     }
 
     public int getQueue() {
         if (isEmpty()) {
             throw new RuntimeException("對列為空，無法取出");
         }
-        front++;
-        return arr[front];
+        int target = arr[front];
+        front = (front + 1) % maxSize;
+        return target;
     }
 
     public void showQueue() {
@@ -44,11 +48,18 @@ public class ArrayQueue {
             return;
         }
 
-        System.out.print("[");
-        for (int item : arr) {
-            System.out.print(item + " ");
+        int[] tmpArr = new int[size()];
+        int tmpArrIndex = 0;
+        for (int i = front; i < front + size(); i++) {
+            tmpArr[tmpArrIndex] = arr[i % maxSize];
+            tmpArrIndex++;
         }
-        System.out.print("]");
+        String arrStr = Arrays.stream(tmpArr).mapToObj(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println("[" + arrStr + "]");
+    }
+
+    public int size() {
+        return (rear + maxSize - front) % maxSize;
     }
 
     public int headQueue() {
@@ -56,6 +67,20 @@ public class ArrayQueue {
             throw new RuntimeException("對列為空");
         }
 
-        return arr[front + 1];
+        return arr[front];
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(3);
+        queue.addQueue(10);
+        queue.addQueue(20);
+        queue.showQueue(); // [10, 20]
+        System.out.println(queue.headQueue()); // 10
+        queue.getQueue();
+        System.out.println(queue.headQueue()); // 20
+        queue.addQueue(30);
+        queue.showQueue(); // [20, 30]
     }
 }
