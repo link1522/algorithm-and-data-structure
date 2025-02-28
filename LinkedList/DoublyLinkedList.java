a4208617e92703d6018e61a17f3168b61a866bdc
Link1515 <h26281660@gmail.com>
2025-02-28 10:42:35 +0800

    add add() in DoublyLinkedList class

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class DoublyLinkedList {
+    private HeroNode2 head = new HeroNode2(0, "", "");
+
+    public static void main(String[] args) {
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+        doublyLinkedList.add(new HeroNode2(1, "hi", "hi2"));
+        doublyLinkedList.add(new HeroNode2(3, "Max", "M"));
+        doublyLinkedList.add(new HeroNode2(2, "Terry", "Tr"));
+        doublyLinkedList.add(new HeroNode2(7, "Terry", "Tr"));
+        doublyLinkedList.add(new HeroNode2(5, "Terry", "Tr"));
+        doublyLinkedList.add(new HeroNode2(6, "Terry", "Tr"));
+        doublyLinkedList.print();
+    }
+
+    // 先考慮加到最後
+    public void add(HeroNode2 node) {
+        HeroNode2 cur = head;
+
+        while (true) {
+            if (cur.next == null) {
+                break;
+            }
+
+            cur = cur.next;
+        }
+
+        cur.next = node;
+        node.pre = cur;
+    }
+
+    public boolean isEmpty() {
+        return head.next == null;
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("鏈表為空");
+        }
+
+        HeroNode2 cur = head.next;
+        while (cur.next != null) {
+            System.out.println(cur);
+            cur = cur.next;
+        }
+        System.out.println(cur);
+    }
+
+}
+
+class HeroNode2 {
+    public int no;
+    public String name;
+    public String nickname;
+    public HeroNode2 next;
+    public HeroNode2 pre;
+
+    public HeroNode2(int no, String name, String nickname) {
+        this.no = no;
+        this.name = name;
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode {\n  no = " + no + ",\n  name = " + name + ",\n  nickname = " + nickname + "\n}";
+    }
+}
