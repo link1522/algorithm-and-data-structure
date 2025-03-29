4dba386141c89206147c3964712b875514fa83cf
Link1515 <h26281660@gmail.com>
2025-03-29 11:57:30 +0800

    add sort by translate in shell sort

--- Sort/ShellSort.java
+++ Sort/ShellSort.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class ShellSort {
     public static void main(String[] args) {
         int[] arr = { 93, 43, 92, 23, 32 };
-        sort(arr);
+        sortByTranslate(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     /*
      * 交換法
      */
-    public static void sort(int[] arr) {
+    public static void sortBySwap(int[] arr) {
         int temp;
 
         for (int gap = arr.length / 2; gap > 0; gap /= 2) {
@@ -23,6 +23,23 @@ public class ShellSort {
                     arr[i] = arr[j];
                     arr[j] = temp;
                 }
+            }
+        }
+    }
+
+    /*
+     * 位移法 (效率高)
+     */
+    public static void sortByTranslate(int[] arr) {
+        for (int gap = arr.length; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                while (j - gap >= 0 && temp < arr[j - gap]) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                arr[j] = temp;
             }
         }
     }
