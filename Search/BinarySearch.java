a8cca442914ae216aed880721910357054f652e2
Link1515 <h26281660@gmail.com>
2025-04-06 17:35:07 +0800

    add search multiple method in BinarySearch

--- Search/BinarySearch.java
+++ Search/BinarySearch.java
@@ -1,10 +1,13 @@
 package Search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 5 };
+        int[] arr = { 1, 3, 3, 3, 5 };
 
-        var result = searchByRecursive(1, arr, 0, arr.length - 1);
+        var result = searchMultipleByRecursive(3, arr, 0, arr.length - 1);
         System.out.println(result);
     }
 
@@ -21,6 +24,35 @@ public class BinarySearch {
             return searchByRecursive(target, arr, left, mid - 1);
         } else {
             return mid;
+        }
+    }
+
+    public static List<Integer> searchMultipleByRecursive(int target, int[] arr, int left, int right) {
+
+        var result = new ArrayList<Integer>();
+
+        if (left > right) {
+            return result;
+        }
+
+        int mid = (left + right) / 2;
+
+        if (target > arr[mid]) {
+            return searchMultipleByRecursive(target, arr, mid + 1, right);
+        } else if (target < arr[mid]) {
+            return searchMultipleByRecursive(target, arr, left, mid - 1);
+        } else {
+            int arrIndex = mid;
+            while (arrIndex - 1 > 0 && arr[arrIndex - 1] == target) {
+                arrIndex--;
+            }
+
+            while (arr[arrIndex] == target) {
+                result.add(arrIndex);
+                arrIndex++;
+            }
+
+            return result;
         }
     }
 }
