7472838de76c7e0b5a25bf363747d8cc714d84fe
Link1515 <h26281660@gmail.com>
2025-05-15 11:18:58 +0800

    create BinarySearchTree

---
+++ BinarySearchTree/BinarySearchTreeDemo.java
@@ -0,0 +1,76 @@
+package BinarySearchTree;
+
+public class BinarySearchTreeDemo {
+    public static void main(String[] args) {
+        int[] arr = { 7, 3, 10, 12, 5, 1, 9 };
+        var binarySearchTree = new BinarySearchTree();
+        for (int num : arr) {
+            binarySearchTree.add(new Node(num));
+        }
+        binarySearchTree.infixOrder();
+    }
+}
+
+class BinarySearchTree {
+    private Node root;
+
+    public void add(Node node) {
+        if (root == null) {
+            this.root = node;
+        } else {
+            root.add(node);
+        }
+    }
+
+    public void infixOrder() {
+        if (root != null)
+            root.infixOrder();
+    }
+}
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    // 遞迴加入，須滿足二叉排序樹
+    public void add(Node node) {
+        if (node == null)
+            return;
+
+        if (node.value < this.value) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                this.left.add(node);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                this.right.add(node);
+            }
+        }
+    }
+
+    public void infixOrder() {
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+
+        System.out.println(this);
+
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Node {\n  value = " + value + "\n}";
+    }
+}
