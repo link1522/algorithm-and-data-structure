d3ff71775d63072b1699d00483c7e3a1d44a4253
Link1515 <h26281660@gmail.com>
2025-05-21 14:41:00 +0800

    add deleteNode() method

--- BinarySearchTree/BinarySearchTreeDemo.java
+++ BinarySearchTree/BinarySearchTreeDemo.java
@@ -2,17 +2,91 @@ package BinarySearchTree;
 
 public class BinarySearchTreeDemo {
     public static void main(String[] args) {
-        int[] arr = { 7, 3, 10, 12, 5, 1, 9 };
+        int[] arr = { 7, 3, 10, 12, 5, 1, 9, 2 };
         var binarySearchTree = new BinarySearchTree();
         for (int num : arr) {
             binarySearchTree.add(new Node(num));
         }
+        // binarySearchTree.infixOrder();
+
+        binarySearchTree.deleteNode(10);
         binarySearchTree.infixOrder();
     }
 }
 
 class BinarySearchTree {
     private Node root;
+
+    public Node search(int value) {
+        if (root == null)
+            return null;
+        return root.search(value);
+    }
+
+    public Node searchParent(int value) {
+        if (root == null)
+            return null;
+        return root.searchParent(value);
+    }
+
+    public void deleteNode(int value) {
+        if (root == null)
+            return;
+
+        var target = search(value);
+        if (target == null)
+            return;
+
+        // 有找到 target 且樹中只有一個 root 節點 (target 即為 root)
+        if (root.left == null && root.right == null) {
+            root = null;
+            return;
+        }
+
+        var parent = searchParent(value);
+        if (target.left == null && target.right == null) {
+            // 如果 target 是葉子節點
+            if (parent.left == target) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        } else if (target.left != null && target.right != null) {
+            // 如果 target 有兩個子節點
+            // 找右子樹中的最小值放到 target
+            var minValue = deleteRightTreeMin(target.right);
+            target.value = minValue;
+        } else {
+            // 如果 target 只有一個子節點
+            Node targetChild;
+            if (target.left != null) {
+                targetChild = target.left;
+            } else {
+                targetChild = target.right;
+            }
+
+            if (parent.left == target) {
+                parent.left = targetChild;
+            } else {
+                parent.right = targetChild;
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param node
+     * @return 以傳入的 node 節點為根找到的最小 value
+     */
+    public int deleteRightTreeMin(Node node) {
+        var target = node;
+        while (target.left != null) {
+            target = target.left;
+        }
+        deleteNode(target.value);
+
+        return target.value;
+    }
 
     public void add(Node node) {
         if (root == null) {
